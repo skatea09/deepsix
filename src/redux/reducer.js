a50db03d24dea6a6e077fc71e20a1c2703f0eacb
Ammon <skatea09@aol.com>
2019-03-02 20:46:04 -0800

    integrates redux

---
+++ src/redux/reducer.js
@@ -0,0 +1,30 @@
+const initialState = {
+  data: []
+};
+
+const rootReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case "UPDATE_NODES_SUCCESS": {
+      const { data } = payload;
+      return Object.assign({}, state, { data });
+    }
+    case "FETCH_NODES_REQUEST":
+    case "ADD_NODE_REQUEST":
+    case "REMOVE_NODE_REQUEST":
+      return Object.assign({}, state, { isFetching: true, error: "" });
+    case "ADD_NODE_SUCCESS":
+    case "REMOVE_NODE_SUCCESS": {
+      return Object.assign({}, state, { isFetching: false, error: "" });
+    }
+    case "ADD_NODE_FAILURE":
+    case "REMOVE_NODE_FAILURE": {
+      const { error } = payload;
+      return Object.assign({}, state, { isFetching: false, error });
+    }
+    default:
+      console.log('hereeee');
+      return state;
+  }
+};
+
+export default rootReducer;
