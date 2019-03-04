f4c14f1da76c2592b7f6ca3bd784f1b1020d66cb
Ammon <skatea09@aol.com>
2019-03-03 23:34:00 -0800

    adds enter key press to input field

--- src/components/NodeName.js
+++ src/components/NodeName.js
@@ -11,11 +11,51 @@ class NodeName extends Component {
     this.textInput.focus();
   }
 
+  blur() {
+    this.textInput.blur();
+  }
+
   updateName = () => {
     const { isEditing, input } = this.state;
     const { name, id, updateName } = this.props;
     this.setState({ isEditing: !isEditing });
     if (input && input !== name) updateName({ id, name: input });
+  };
+
+  Input = () => (
+    <input
+      className="outline-none border rounded-sm mb-1 pl-2"
+      onBlur={this.updateName}
+      onChange={e => this.setState({ input: e.target.value })}
+      onKeyPress={e => {
+        if (e.key === "Enter") {
+          e.preventDefault();
+          this.blur();
+        }
+      }}
+      ref={input => {
+        this.textInput = input;
+      }}
+    />
+  );
+
+  Name = () => {
+    const { name, id } = this.props;
+    return (
+      <Link to={`/${id}`} className="no-underline text-black hover:underline">
+        {name}
+      </Link>
+    );
+  };
+
+  Pencil = () => {
+    const { isEditing } = this.state;
+    return (
+      <i
+        className="fas fa-pencil-alt pin-r pl-1 cursor-pointer text-sm text-black px-2"
+        onClick={() => this.setState({ isEditing: !isEditing })}
+      />
+    );
   };
 
   componentDidUpdate() {
@@ -24,38 +64,16 @@ class NodeName extends Component {
   }
 
   render() {
-    const { id, name } = this.props;
     const { isEditing, hovering } = this.state;
+    const { Input, Name, Pencil } = this;
     return (
       <div
         className="flex relative"
         onMouseEnter={() => this.setState({ hovering: !hovering })}
         onMouseLeave={() => this.setState({ hovering: !hovering })}
       >
-        {isEditing ? (
-          <input
-            className="outline-none border rounded-sm mb-1"
-            onBlur={this.updateName}
-            onChange={e => this.setState({ input: e.target.value })}
-            ref={input => {
-              this.textInput = input;
-            }}
-          />
-        ) : (
-          <Link
-            to={`/${id}`}
-            className="no-underline text-black hover:underline"
-          >
-            {name}
-          </Link>
-        )}
-        {hovering &&
-          !isEditing && (
-            <i
-              className="fas fa-pencil-alt pin-r pl-1 cursor-pointer text-sm text-black px-2"
-              onClick={() => this.setState({ isEditing: !isEditing })}
-            />
-          )}
+        {isEditing ? <Input /> : <Name />}
+        {hovering && !isEditing && <Pencil />}
       </div>
     );
   }
